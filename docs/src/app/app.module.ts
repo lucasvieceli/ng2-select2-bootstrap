3f515f4cf6af497789870934176606988ca1d14b
lucas vieceli <vieceli46@hotmail.com>
2017-02-09 09:01:29 -0200

    adicionado angular2-seed para a pagina de exemplo

---
+++ docs/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true })
+  ],
+  providers: [
+    
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+
+}
