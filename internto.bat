b148a821a91eab912f3441d8aa8b6aba67b62a52
lucas vieceli <vieceli46@hotmail.com>
2017-02-20 17:33:12 -0300

    no message

--- internto.bat
+++
@@ -1,9 +0,0 @@
-
-echo before
-CMD /C npm run tsc
-
-echo after
-copy index.d.ts C:\xampp\htdocs\ng2-select2-bootstrap\docs\src\node_modules\ng2-select2-bootstrap
-copy index.js C:\xampp\htdocs\ng2-select2-bootstrap\docs\src\node_modules\ng2-select2-bootstrap
-copy index.js.map C:\xampp\htdocs\ng2-select2-bootstrap\docs\src\node_modules\ng2-select2-bootstrap
-xcopy src C:\xampp\htdocs\ng2-select2-bootstrap\docs\src\node_modules\ng2-select2-bootstrap\src\ /y /s
